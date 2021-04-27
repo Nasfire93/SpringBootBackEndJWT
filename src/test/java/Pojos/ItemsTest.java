9bed38f9bc8440d663edbe1f0781238e7b739b6b
Alejandro.Carrillo <alejandro.carrillo@bitboxonline.com>
2021-04-27 08:37:07 +0100

    Ya logea

---
+++ src/test/java/Pojos/ItemsTest.java
@@ -0,0 +1,74 @@
+package Pojos;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemsTest {
+    Items item = new Items();
+
+    @Test
+    void getItemsId() {
+
+    }
+
+    @Test
+    void setItemsId() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void setPrice() {
+    }
+
+    @Test
+    void getState() {
+    }
+
+    @Test
+    void setState() {
+    }
+
+    @Test
+    void getSuppliers() {
+    }
+
+    @Test
+    void setSuppliers() {
+    }
+
+    @Test
+    void getPriceReductions() {
+    }
+
+    @Test
+    void setPriceReductions() {
+    }
+
+    @Test
+    void getCreationDate() {
+    }
+
+    @Test
+    void setCreationDate() {
+    }
+
+    @Test
+    void getCreator() {
+    }
+
+    @Test
+    void setCreator() {
+    }
+}
